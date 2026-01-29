800f059326fabf74991ab42177d2a8974ed7647a
Manuel Ytuza <alejo.ytuza@gmail.com>
2026-01-29 11:25:33 -0500

    Initial commit

---
+++ 1.js
@@ -0,0 +1,12 @@
+let fatherName = "Manuel";
+let doutherName = "Alice";
+let secondDoutherName = "Emile ";
+let nickName = "exmoxnix";
+let feelingFather = "proud";
+console.log (`My name is ${fatherName}. My douther's name is ${doutherName}, but her nickname is ${nickName}. I am very ${feelingFather} of her!`);
+
+function successfulFather() {
+    return `${fatherName} is a successful father because his douther ${doutherName} is very clever and kind.`;
+}
+
+console.log (successfulFather()); 
