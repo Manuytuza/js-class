3f22e31c938e59fbab95ec6ce31318eec98c83b2
Manuel Ytuza <alejo.ytuza@gmail.com>
2026-03-09 21:05:51 -0500

    fin de js, queda pendiente ultimo capitulos

--- guia.js
+++ guia.js
@@ -959,3 +959,59 @@ promesaPokemon
 
   //..............................................................................
   // 07/03/2026, 08:52:46 p. m. inciamos repaso 
+  //09/03/2026, 07:58:47 p. m. volvemos unos horas pm 
+  /**
+   * dejamos algunos conceptos para detallar despues 
+   * Peticiones HTTP en JavaScript: Fetch, GET, POST y DELETE
+ const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("#posts-container");
+
+function sendHTTPRequest(method, url, data) {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+async function fecthPosts() {
+  const responseData = await sendHTTPRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log(responseData);
+  const listOfPosts = responseData;
+
+  for (const post of listOfPosts) {
+    const postContainer = document.createElement("article");
+    postContainer.id = post.id;
+    postContainer.classList.add("post-item");
+
+    const title = document.createElement("h2");
+    title.textContent = post.title;
+
+    const body = document.createElement("p");
+    body.textContent = post.body;
+
+    const button = document.createElement("button");
+    button.textContent = "DELETE Content";
+
+    postContainer.append(title);
+    postContainer.append(body);
+    postContainer.append(button);
+
+    listElement.append(postContainer);
+  }
+}
+
+fetchButton.addEventListener("click", fecthPosts);
+
+QUEDA PENTIENTE TERMINAR CURSO DE JS FETCH PLATZY
+   */
